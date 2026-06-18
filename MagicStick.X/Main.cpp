b5dc7bfcc576a3cfaa6817b67026a4b8444c4c97
nbures31 <koki.halloween@gmail.com>
2026-06-19 00:26:49 +0900

    単発推論モードに変更
    連続推論モードは音声データでなければ使えなかったため、単発推論モードに変更
    ⇒推論可能になったが、すべての結果が異常と判断されている
    
    推論可能となったため、簡易的な結果を出力する機能を追加

--- MagicStick.X/Main.cpp
+++ MagicStick.X/Main.cpp
@@ -17,7 +17,7 @@ extern "C"{
 #include "edge-impulse-sdk/classifier/ei_run_classifier.h"
 
 using namespace std;
-static float eiAccBuffer[EI_CLASSIFIER_SLICE_SIZE * EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME] = {0};
+static float eiAccBuffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE] = {0};
                                                                                 // EI(Edge Impulse で生成したAI)が使用するための加速度センサのデータ(2000ms/4)のバッファ
 char tmpText[50];
 void serial_printf(char* text);
@@ -30,7 +30,7 @@ int main(int argc, char** argv) {
     ei_impulse_result_t result;                                                 // 結果格納用
     bool debugFlg = false;                                                              // デバッグ出力有無
      // コールバック関数と必要なデータ数を設定
-    signal.total_length = EI_CLASSIFIER_SLICE_SIZE * EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME;
+    signal.total_length = EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE;
     signal.get_data     = &get_signal_data;
     
     ADC_Enable();                                                               // ADC(アナログ/デジタル)変換を開始
@@ -39,13 +39,18 @@ int main(int argc, char** argv) {
     while(1){
         AccelRead();                                                            // 加速度センサ読み取り
         
-        EI_IMPULSE_ERROR res = run_classifier_continuous(&signal, &result, debugFlg); // 連続推論
+        EI_IMPULSE_ERROR res = run_classifier(&signal, &result, debugFlg);
 
         if (res == EI_IMPULSE_OK) {
-            // 結果をシリアル表示
-            display_results(&ei_default_impulse, &result);
-        }else{
-            sprintf(tmpText, "fail %d\r\n\0", res);
+            if(result.anomaly > 0.3){
+                sprintf(tmpText, "anomaly %f\r\n", result.anomaly);
+                serial_printf(tmpText);
+            }else{
+                sprintf(tmpText, "classify     attack :%f  circle :%f  poke :%f\r\n", result.classification[0].value, result.classification[1].value, result.classification[2].value);
+                serial_printf(tmpText);
+            }
+        } else {
+            sprintf(tmpText, "fail %d\r\n", res);
             serial_printf(tmpText);
         }
     }
@@ -57,15 +62,26 @@ int main(int argc, char** argv) {
 //  加速度センサデータ更新
 ////////////////////////////////////////////////////////////
 void AccelRead(){
-    size_t samples_per_slice = EI_CLASSIFIER_SLICE_SIZE;
-    for (size_t i = 0; i < samples_per_slice; i++) {
+    int total_floats = EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE;                      // 300
+    int slice_floats = EI_CLASSIFIER_SLICE_SIZE * EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME; // 75
+    int shift_floats = total_floats - slice_floats;                             // 225
+    
+    // 1. 古いデータを左にズラす（過去の記憶を保持）
+    for (int i = 0; i < shift_floats; i++) {
+        eiAccBuffer[i] = eiAccBuffer[i + slice_floats];
+    }
+    
+    for (size_t i = 0; i < EI_CLASSIFIER_SLICE_SIZE; i++) {
         // 次のサンプリング時刻を計算 (20ms間隔)
         uint64_t next_tick = ei_read_timer_ms() + EI_CLASSIFIER_INTERVAL_MS;
 
+        // 書き込む基準の位置（225番目以降）
+        int base_idx = shift_floats + (i * 3);
+        
         // 各軸を読み取ってバッファへ格納
-        eiAccBuffer[(i * 3) + 0] = (float)AccelReadSub(PIN_ACC_X);
-        eiAccBuffer[(i * 3) + 1] = (float)AccelReadSub(PIN_ACC_Y);
-        eiAccBuffer[(i * 3) + 2] = (float)AccelReadSub(PIN_ACC_Z);
+        eiAccBuffer[base_idx + 0] = (float)AccelReadSub(PIN_ACC_X);
+        eiAccBuffer[base_idx + 1] = (float)AccelReadSub(PIN_ACC_Y);
+        eiAccBuffer[base_idx + 2] = (float)AccelReadSub(PIN_ACC_Z);
         
         // 20ms経過するまで待機
         while (ei_read_timer_ms() < next_tick);
@@ -128,9 +144,25 @@ void serial_printf(char* text){
 /////////////////////////////////////////////////////////////
 void ei_printf(const char *format, ...) {
     char print_buf[256];
+    char tx_buf[512]; // \rを追加するための、少し大きめの送信用バッファ
     va_list args;
+    
+    // 1. フォーマット通りに文字を組み立てる
     va_start(args, format);
     vsnprintf(print_buf, sizeof(print_buf), format, args);
     va_end(args);
-    serial_printf(print_buf);
+
+    // 2. Linux式(\n)をWindows/シリアル通信式(\r\n)に変換する
+    int j = 0;
+    for (int i = 0; print_buf[i] != '\0' && j < 510; i++) {
+        // もし「\n」を見つけたら、その直前に「\r」を割り込ませる
+        if (print_buf[i] == '\n') {
+            tx_buf[j++] = '\r'; 
+        }
+        tx_buf[j++] = print_buf[i];
+    }
+    tx_buf[j] = '\0'; // 最後に必ず終端文字を入れる
+
+    // 3. 綺麗な改行に変換された文字を送信！
+    serial_printf(tx_buf);
 }
